3fc2de8fc77df86990727bdb143a18f98a3273cd
clonn <clonncd@gmail.com>
2016-10-22 12:28:34 +0800

    remove commit

--- webpack.config.js
+++ webpack.config.js
@@ -35,12 +35,3 @@ var config = module.exports = {
 		new LiveReloadPlugin(liveReloadOption)
 	]
 };
-// var WebpackDevServer = require("webpack-dev-server");
-
-// // var config = require("./webpack.config.js");
-// config.entry.app.unshift("webpack-dev-server/client?http://localhost:8080/", "webpack/hot/dev-server");
-// var compiler = webpack(config);
-// var server = new WebpackDevServer(compiler, {
-//   hot: true
-// });
-// server.listen(8080);
