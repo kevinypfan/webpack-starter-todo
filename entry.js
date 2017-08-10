0c000d136c6e10825d2ca32d087ebd59f687545f
KevinFan <kevin_ypfan@yahoo.com.tw>
2017-08-10 13:57:03 +0800

    work

--- entry.js
+++ entry.js
@@ -1,15 +1,18 @@
 import $ from 'jquery';
 import getForm from "./lib/form.data.js";
 import insertData from "./lib/insertData.js";
-
+import deleteData from "./lib/deleteData.js";
 let form = $("form");
+
 
 form.on('submit', function (e) {
 	e.preventDefault();
 	var value = getForm(e);
 	insertData(value);
 	alert(`Add item: ${value}`);
+    deleteData(value);
 });
+
 
 
 
